4113cd98e770e835027387670394dbaf622cb83b
deniotokiari <deniotokiari@gmail.com>
2015-07-31 01:02:43 +0300

    delete old files

--- plugins/contrl.py
+++
@@ -1,5 +0,0 @@
-#coding: utf-8
-from owner import access
-def run(text,sock):
-	if text.msg.words(0)=='$c' and access(text.sender())==True:
-		sock().send('PRIVMSG '+text.msg().split('$c')[1]+'\r\n')
--- plugins/owner.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-def access(user):
-		file=open('lib/owner','r')
-		str=file.read()
-		file.close()
-		host=user.split('@')[1]
-		if host in str:return True
-		else:return False
-def run(text,sock):pass
